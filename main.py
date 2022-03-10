f929919e77d5a7cf73ff6a8880d29d6582b82559
xsjk <xsjk17@outlook.com>
2022-03-10 21:10:17 +0800

    2.3

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-TXT = '''The traditional culture, which is, of course, mainly literary, is behaving like a state whose power is rapidly declining – standing on its precarious dignity, spending far too much energy on Alexandrine intricacies, occasionally letting fly in fits of aggressive pique quite beyond its means, too much on the defensive to show any generous imagination to the forces which must inevitably reshape it. Whereas the scientific culture is expansive, not restrictive, confident at the roots, the more confident after its bout of Oppenheimerian self-criticism, certain that history is on its side, impatient, intolerant, creative rather than critical, good- natured and brash. Neither culture knows the virtues of the other; often it seems they deliberately do not want to know. The resentment which the traditional culture feels for the scientific is shaded with fear; from the other side, the resentment is not shaded so much as brimming with irritation. When scientists are faced with an expression of the traditional culture, it tends (to borrow Mr William Cooper’s eloquent phrase) to make their feet ache. It does not need saying that generalizations of this kind are bound to look silly at the edges. There are a good many scientists indistinguishable from literary persons, and vice versa. Even the stereotype generalizations about scientists are misleading without some sort of detail – e.g. the generalizations that scientists as a group stand on the political Left. This is only partly true. A very high proportion of engineers is almost as conservative as doctors; of pure scientists, the same would apply to chemists. It is only among physicists and biologists that one finds the Left in strength. If one compared the whole body of scientists with their opposite numbers of the traditional culture (writers, academics, and so on), the total result might be a few per cent more towards the Left wing, but not more than that. Nevertheless, as a first approximation, the scientific culture is real enough and so is its difference from the traditional. For anyone like myself, by education a scientist, by calling a writer, at one time moving between groups of scientists and writers in the same evening, the difference has seemed dramatic.'''
 FLAG = False
+import sys
+
+if len(sys.argv)==1:
+    input('请将文本文件拖动到此文件上运行')
+    exit()
+try:
+    with open(sys.argv[1],encoding='utf-8') as f:
+    	TXT = f.read().strip()
+except:
+    input("打开文件失败")
+    exit()
+else:
+    print("打开文件成功")
+    print(TXT)
 
 import keyboard
 
